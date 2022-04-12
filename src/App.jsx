8a9d3480cdc8725664fdbe459e4bab2b8308bd3b
ibilalchaudhary <bilalchaudhary478@gmail.com>
2022-04-13 00:02:38 +0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./screens/Home";
 import "./App.scss";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,138 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 export default function Header() {
-  return <div>Header</div>;
+  const [isHeaderOpen, setIsHeaderOpen] = useState(true);
+
+  useEffect(() => {
+    if (window.innerWidth > 800) {
+      setIsHeaderOpen(true);
+    } else {
+      setIsHeaderOpen(false);
+    }
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 800) {
+        setIsHeaderOpen(true);
+      } else {
+        setIsHeaderOpen(false);
+      }
+    });
+  }, []);
+
+  const navigate = useNavigate();
+  return (
+    <div className="header">
+      <div className="header__wrapper">
+        <Link
+          to="/"
+          onClick={() => {
+            document.getElementById("home").checked = true;
+          }}
+        >
+          <img src={logo} className="brand__logo" />
+        </Link>
+        <button
+          className="header__mobile__btn"
+          onClick={() => {
+            isHeaderOpen ? setIsHeaderOpen(false) : setIsHeaderOpen(true);
+          }}
+        >
+          {isHeaderOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-x"
+            >
+              <line x1="18" y1="6" x2="6" y2="18"></line>
+              <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-menu"
+            >
+              <line x1="3" y1="12" x2="21" y2="12"></line>
+              <line x1="3" y1="6" x2="21" y2="6"></line>
+              <line x1="3" y1="18" x2="21" y2="18"></line>
+            </svg>
+          )}
+        </button>
+        {isHeaderOpen ? (
+          <div className="nav">
+            <div className="nav__link">
+              <input
+                type="radio"
+                id="home"
+                defaultChecked={true}
+                className="nav__link__input"
+                name="nav__link__input"
+                onClick={() => {
+                  navigate("/");
+                }}
+              />
+              <div className="nav__link__content">Home</div>
+            </div>
+            <div className="nav__link">
+              <input
+                type="radio"
+                id="about"
+                className="nav__link__input"
+                name="nav__link__input"
+                onClick={() => {
+                  navigate("/");
+                }}
+              />
+              <div className="nav__link__content">Services</div>
+            </div>
+            <div className="nav__link">
+              <input
+                type="radio"
+                id="about"
+                className="nav__link__input"
+                name="nav__link__input"
+                onClick={() => {
+                  navigate("/");
+                }}
+              />
+              <div className="nav__link__content">About Us</div>
+            </div>
+            <div className="nav__link">
+              <input
+                type="radio"
+                id="about"
+                className="nav__link__input"
+                name="nav__link__input"
+                onClick={() => {
+                  navigate("/");
+                }}
+              />
+              <div className="nav__link__content">Contact Us</div>
+            </div>
+            <Link to="/" className="nav__link__login">
+              Login
+            </Link>
+            <Link to="/" className="nav__link__register">
+              Sign Up
+            </Link>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 }
