0ccadeae64b12f6fa75941ca8fe571c21a1f4249
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-04-12 23:04:38 +0500

    base done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./screens/Home";
+import "./App.scss";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
 }
