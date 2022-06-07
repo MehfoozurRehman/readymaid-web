178d12c5b1fe6fb2b76e5779f331228cbba5de2b
Bilal Shabbir <102783392+IBilalShabbir@users.noreply.github.com>
2022-06-07 17:26:26 +0500

    footer__done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/New__header";
 import Home from "./screens/Home";
+
 import Benefit from "./screens/Benefit__to__Customers";
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
---
+++ src/components/Product__section.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Product__page() {
+  return (
+    <div className="product__section">
+      <div className="product__section__container"></div>
+    </div>
+  );
+}
