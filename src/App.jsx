90a81545b177fc3a04cd231c22dae6baade6bca2
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-07-02 13:24:05 +0500

    start from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Footer from "./components/Footer";
-import Header from "./components/New__header";
-import Home from "./screens/Home";
-
-import Benefit from "./screens/Benefit__to__Customers";
-import "./App.scss";
 import { Route, Routes } from "react-router-dom";
-import Login from "./screens/Login";
-import Signup from "./screens/signup";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./screens/Home";
+import "./App.scss";
 
 export default function App() {
   return (
@@ -15,14 +11,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/benefits" element={<Benefit />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </>
-   
-    
   );
-  
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,26 +1,5 @@
 import React from "react";
-import Simplified from "../components/Simplified";
-import Solution from "../components/Solution";
-import Carouselslide from "../components/Carousel";
-import About__Section from "../components/About__Section";
-import Industry from "../components/Industry";
-import Qualityservices from "../components/Qualityservices";
-import Accordian from "../components/Accordian";
-import HomeSection from "../components/HomeSection";
-import Header from "../components/Header";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <HomeSection />
-      <Carouselslide />
-      <Solution />
-      <About__Section />
-      <Simplified />
-      <Industry />
-      <Qualityservices />
-      <Accordian />
-    </>
-  );
+  return <></>;
 }
