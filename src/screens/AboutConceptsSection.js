b9a113f08089fa404c0ee325e95689eb6ab282d6
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-07-03 17:14:25 +0500

    AboutConceptsSection done

---
+++ src/screens/AboutConceptsSection.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { readymaidconcepts } from "../assets";
+
+export function AboutConceptsSection({}) {
+  const readymaidConceptsData = [
+    "It is not about creating a marketplace or service providing platform but   creating an industry that doesn’t exist at all.",
+    "It is about empowering the most neglected segment of the society.",
+    "It is about providing a corporate exposure and working environment to the ones who can never think about it.",
+    "It is about providing job security to the ones who find it difficult even to get (job) one. ",
+    "It is not about gaining from the society but giving back to it.",
+    "It is not about calling people for the job rather reaching out to them for it.",
+    "It is about creating a platform where anyone can come to find a job.",
+    "It is about creating an environment where people are not judged by their qualification and skill sets but their identity i.e. their existence.",
+    "It is not about finding but making the right resource.",
+  ];
+  return (
+    <div className="about__concepts">
+      <img
+        src={readymaidconcepts}
+        alt="readymaidconcepts"
+        className="about__concepts__img"
+      />
+      <div className="about__concepts__content">
+        <div className="about__concepts__content__heading">
+          The concept of Readymaid is simple yet powerful
+        </div>
+        <div className="about__concepts__content__list">
+          {readymaidConceptsData.map((item) => (
+            <div
+              className="about__concepts__content__list__item"
+              key={JSON.stringify(item)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/screens/AboutUs.js
+++ src/screens/AboutUs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { readymaidconcepts } from "../assets";
+import { AboutConceptsSection } from "./AboutConceptsSection";
 import { AboutJumbotronSection } from "./AboutJumbotronSection";
 import { AboutKeyFactorsSection } from "./AboutKeyFactorsSection";
 import { AboutOurMissionSection } from "./AboutOurMissionSection";
@@ -12,43 +12,5 @@ export default function AboutUs() {
       <AboutKeyFactorsSection />
       <AboutOurMissionSection />
     </>
-  );
-}
-
-function AboutConceptsSection({}) {
-  const readymaidConceptsData = [
-    "It is not about creating a marketplace or service providing platform but   creating an industry that doesn’t exist at all.",
-    "It is about empowering the most neglected segment of the society.",
-    "It is about providing a corporate exposure and working environment to the ones who can never think about it.",
-    "It is about providing job security to the ones who find it difficult even to get (job) one. ",
-    "It is not about gaining from the society but giving back to it.",
-    "It is not about calling people for the job rather reaching out to them for it.",
-    "It is about creating a platform where anyone can come to find a job.",
-    "It is about creating an environment where people are not judged by their qualification and skill sets but their identity i.e. their existence.",
-    "It is not about finding but making the right resource.",
-  ];
-  return (
-    <div className="about__concepts">
-      <img
-        src={readymaidconcepts}
-        alt="readymaidconcepts"
-        className="about__concepts__img"
-      />
-      <div className="about__concepts__content">
-        <div className="about__concepts__content__heading">
-          The concept of Readymaid is simple yet powerful
-        </div>
-        <div className="about__concepts__content__list">
-          {readymaidConceptsData.map((item) => (
-            <div
-              className="about__concepts__content__list__item"
-              key={JSON.stringify(item)}
-            >
-              {item}
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
   );
 }
